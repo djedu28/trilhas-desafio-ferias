b9b996294247dd7b11406d2d3485a819d7061481
Eduardo Santos <43933482+djedu28@users.noreply.github.com>
2022-01-04 21:44:56 -0300

    simplificando importação de componente

--- js/script.js
+++ js/script.js
@@ -15,3 +15,24 @@ function laodCss(pathname){
 	  .appendChild(css);
 
 }
+
+function laodJs(pathname){
+	
+	const js = $("<link>", {
+	  "rel" : "stylesheet",
+	  "type" :  "text/javascript",
+	  "href" : pathname
+	})[0];
+
+	document
+	  .getElementsByTagName("head")[0]
+	  .appendChild(js);
+
+}
+function laodComp(name,{html,js,css}){
+	if ( html||true ) renderHtml(`#${name}`,`./componentes/${name}/${name}.html`);
+	if ( css ||true ) laodCss(`./componentes/${name}/${name}.css`);
+	if ( js  ||true ) laodJs(`./componentes/${name}/${name}.js`);
+}
+
+ 
