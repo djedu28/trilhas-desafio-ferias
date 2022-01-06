e3695f967f6aeac124d25f0a18015b64f2f00be3
Eduardo Santos <43933482+djedu28@users.noreply.github.com>
2022-01-06 14:16:34 -0300

    Separando gerenciador de rotas  em routes.js
    separando gerenciador de rotas em arquivo separado routes.js

---
+++ routes.js
@@ -0,0 +1,20 @@
+
+$(function () {
+	laodComp("header",{js:false}) // js=false
+	//laodComp("main",{js:false})
+	laodComp("footer",{js:false})
+})
+
+microroute("/pagina2",()=>{
+	laodPage("pagina2",{js:false})
+})
+
+microroute("/",()=>{
+	laodPage("home",{js:false})
+})
+
+microroute("*",()=>{
+	laodPage("erro404",{js:false})
+})
+
+microroute.run()
