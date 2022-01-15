0fb34321ea18bd244f427ce7ec253d0a679c6705
Eduardo Santos <43933482+djedu28@users.noreply.github.com>
2022-01-15 09:42:19 -0300

    Ativando regras de comparação

--- pages/game/game.js
+++ pages/game/game.js
@@ -1,9 +1,9 @@
 
 function isNumber(n) {
-	return !isNaN(parseFloat(n)) && isFinite(n);
+    return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
-function criarBox(tipo,{value}){
+function criarBox(tipo,{value,id,i,j}){
 	const box = document.createElement('div');
 	
 	
@@ -22,6 +22,10 @@ function criarBox(tipo,{value}){
 			autocomplete  = "off"
 			maxlength = 1
 			size = 1
+			onblur = "testeLetra(event)"
+			id = ${id}
+			i = ${i}
+			j = ${j}
 
 		/>`
 	}
@@ -48,27 +52,50 @@ const mapa = [
 	[" " ," " ," " ," " ," " ," " ," " ,"S" ," " ," " ," " ," " ," " ," " ," " ," "] , // 14
 ]
 
-function Start(mapa){
-	const mapaHTML = document.getElementsByClassName("mapa")[0];
-	mapa.forEach((linha,i) => {
-		const linhaHTML = document.createElement('section');
-		//const fragment = document.createDocumentFragment();	
+const mapaHTML = document.getElementsByClassName("mapa")[0];
+mapa.forEach((linha,i) => {
+	const linhaHTML = document.createElement('section');
+	//const fragment = document.createDocumentFragment();	
 
-		linha.forEach((box,j)=>{
-			let tipo = 
-				isNumber(box) ? "N"  // numero de incide
-				: box===" " ? "L" 	 // caixa limpa
-				: box==="*" ? "BK"   // caixa bloqueada
-				: "A"	             // caixa com input ativado
-				//: "A-BK";			 // caixa bloqueada com texto
-				// modificar o acima para apenas A e terá o input ativados
-			const boxHTML = criarBox(tipo,{value:box})
-			boxHTML.id = `L${i}_C${j}`
-			linhaHTML.appendChild(boxHTML)
-		})
-		
-		mapaHTML.appendChild(linhaHTML)
-
+	linha.forEach((box,j)=>{
+		let tipo = 
+			isNumber(box) ? "N"  // numero de incide
+			: box===" " ? "L" 	 // caixa limpa
+			: box==="*" ? "BK"   // caixa bloqueada
+			: "A"	             // caixa com input ativado
+			//: "A-BK";			 // caixa bloqueada com texto
+			// modificar o acima para apenas A e terá o input ativados
+		const boxHTML = criarBox(tipo,{value:box,id:`L${i}_C${j}`,i,j})
+		boxHTML.id = `div_L${i}_C${j}`
+		linhaHTML.appendChild(boxHTML)
 	})
+	
+	mapaHTML.appendChild(linhaHTML)
 
+})
+
+// console.log("mapa",{mapa})
+
+// console.log(`
+// modificar linha 147 de "A-BK" para apenas "A" e terá o input ativados
+
+// agora basta pecorrer todos os box comparando o input.value com mapa
+
+// cada box possui um id referente a linha e coluna
+// exemplo id = "L1C2" // linha 1 e coluna 2
+// `);
+
+function testeLetra(e){
+	e.target.value = e.target.value.toUpperCase()
+	console.log("testeLetra",{e});
+	const value = e.target.value;
+	
+	const i = e.target.getAttribute("i")
+	const j = e.target.getAttribute("j")
+	if (value === mapa[i][j]){
+		e.target.disabled=1
+	}
+	
+	// <!-- box.classList.add(tipo) -->
 }
+
